8cc350763d755ac33d47e4ee34383f858e62ef1f
Aleksandra <75786246+Simalexa@users.noreply.github.com>
2021-08-04 17:01:09 +0300

    Add files via upload

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -1,27 +1,27 @@
-#include <iostream>
 #include "Queue.h"
 
 int main()
 {
-    Queue q1;
-    q1.OpenFromFile();
-    q1.print();
-    q1.DeleteQueue();
+    Queue<int> q1;
     q1.print();
     q1.add(1);
     q1.add(2);
     q1.add(3);
     q1.print();
-    Queue q2;
+    Queue<int> q2;
     q2.add(4);
     q2.add(5);
     q2.add(6);
     q2.print();
-    Queue q3=q1 + q2;
+    Queue<int> q3 = q1 + q2;
     q3.print();
     q3.flip();
     q3.print();
-    q3.FindElement(6);
-    q3.WriteInFile();
+    Queue<char> q4;
+    q4.add('a');
+    Queue<char> q5;
+    q4.add('b');
+    Queue<char> q6 = q4 + q5;
+    q4.print();
     return 0;
 }
--- Queue.h
+++ Queue.h
@@ -1,23 +1,99 @@
 #ifndef QUEUE_H
 #define QUEUE_H
+#include <vector>
+#include <iostream>
+#include <fstream>
 
+template <typename T>
 class Queue
 {
 private:
-	int size;
-	int* q;
+	int sizeOfq;
 public:
-	Queue();
+	std::vector<T> q;
 	~Queue();
-	void add(int a);
+	Queue();
+	void add(T a);
 	void print();
 	void deleteIndex(int b);
-	void flip();
-	Queue operator+(Queue obj);
-	void FindElement(int c);
-	void WriteInFile();
+	void FindElement(T c);
 	void OpenFromFile();
 	void DeleteQueue();
+	void WriteInFile();
+	void flip();
+	Queue operator+(Queue obj);
 };
-
+template <typename T> Queue<T>::~Queue()
+{
+	q.clear();
+}
+template <typename T> Queue<T>::Queue()
+{
+}
+template <typename T> void Queue<T>::add(T a)
+{
+	q.push_back(a);
+}
+template <typename T> void Queue<T>::print()
+{
+	for (int i = 0; i < q.size(); i++)
+		std::cout << q[i] << " ";
+	std::cout << std::endl;
+}
+template <typename T> void Queue<T>::deleteIndex(int b)
+{
+	if (b > q.size())
+		std::cout << "Error" << std::endl;
+	else
+		q.erase(q.begin() + b);
+}
+template <typename T> void Queue<T>::FindElement(T c)
+{
+	auto n = std::find(q.begin(), q.end(), c);
+	std::cout << "Number in row "  << *n << std::endl;
+}
+template <typename T> void Queue<T>::OpenFromFile()
+{
+	std::ifstream File;
+	T a;
+	File.open("qu.txt");
+	while (!File.eof())
+	{
+		File >> a;
+		q.push_back(a);
+	}
+	q.pop_back();
+}
+template <typename T> void Queue<T>::DeleteQueue()
+{
+	q.clear();
+}
+template <typename T> void Queue<T>::WriteInFile()
+{
+	std::ofstream File;
+	File.open("qu.txt");
+	for (int i = 0;i < q.size();i++)
+		File << q[i] << " ";
+	File.close();
+}
+template <typename T> void Queue<T>::flip()
+{
+	std::vector<T> q0;
+	for (int i = 0; i < q.size();i++)
+		q0.push_back(q[i]);
+	for (int i = 0;i < q.size();i++)
+	{
+		q[i] = q0[q.size() - i - 1];
+	}
+	q0.clear();
+}
+template <typename T> Queue<T> Queue<T>::operator+(Queue obj)
+{
+	Queue<T> res;
+	for (int i = 0; i < q.size(); i++)
+		res.add(q[i]);
+	for (int i = 0; i < obj.q.size(); i++)
+		res.add(obj.q[i]);
+	return res;
+}
 #endif
